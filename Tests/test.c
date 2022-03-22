1fea4737687d3e8f04f61b88b0f78f6e1059fd34
Carlos Bentin <cbentin12@gmail.com>
2022-03-22 10:34:54 -0500

    Task 1 added - insert node at left of parent

--- Tests/test.c
+++ Tests/test.c
@@ -33,6 +33,16 @@ int main(int argc, char **argv)
 		binary_tree_print(root);
 		break;
 
+	case 1:
+		root = binary_tree_node(NULL, 98);
+		root->left = binary_tree_node(root, 12);
+		root->right = binary_tree_node(root, 402);
+		binary_tree_print(root);
+		printf("\n");
+		binary_tree_insert_left(root->right, 128);
+		binary_tree_insert_left(root, 54);
+		binary_tree_print(root);
+		break;
 	default:
 		break;
 	}

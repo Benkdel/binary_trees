09258c30c27a8b2efa79a8f9ee219bea719600a6
Carlos Bentin <cbentin12@gmail.com>
2022-03-22 14:44:36 -0500

    Task 8 added - Traverse BT in post order

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * void binary_tree_postorder - goes through a binary tree using in-order traversal
+ * @tree: pointer to a binary trree
+ * @func: pointer to a function to be aplied for each node
+ * Return: none - void function
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	binary_tree_postorder(tree->left, func);
+
+	binary_tree_postorder(tree->right, func);
+
+	func(tree->n);
+}
--- Tests/test.c
+++ Tests/test.c
@@ -120,6 +120,17 @@ int main(int argc, char **argv)
 		binary_tree_print(root);
 		binary_tree_inorder(root, &print_num);
 		break;
+	case 8:
+		root = binary_tree_node(NULL, 98);
+		root->left = binary_tree_node(root, 12);
+		root->right = binary_tree_node(root, 402);
+		root->left->left = binary_tree_node(root->left, 6);
+		root->left->right = binary_tree_node(root->left, 56);
+		root->right->left = binary_tree_node(root->right, 256);
+		root->right->right = binary_tree_node(root->right, 512);
+		binary_tree_print(root);
+		binary_tree_postorder(root, &print_num);
+		break;
 
 	default:
 		break;

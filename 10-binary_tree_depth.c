07c875f255eece03bd67f4b613a65d9d89dde86e
Carlos Bentin <cbentin12@gmail.com>
2022-03-23 09:40:28 -0500

    Task 10 added - find depth of node, not using recursion

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,31 +1,22 @@
 #include "binary_trees.h"
 
 /**
- * node_depth - measures depth of node
- * @tree: pointer to the node to be measured
- * @target_node: value of node to be measured, to be identified
- * Return: depth of node
- */
-size_t node_depth(const binary_tree_t *tree, int target_node)
-{
-	size_t result = 0;
-
-	(void)result;
-	(void)tree;
-	(void)target_node;
-
-	return (1);
-}
-
-/**
  * binary_tree_depth - wrapper function to call node_depth recursively
  * @tree: pointer to the node to be measured
  * Return: depth of the node
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	size_t result = 0;
+
 	if (tree == NULL)
 		return (0);
 
-	return (node_depth(tree, tree->n));
+	while (tree->parent)
+	{
+		result++;
+		tree = tree->parent;
+	}
+
+	return (result);
 }
